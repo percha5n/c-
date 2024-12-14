3f57a9a09b0ff026ec64593a5ca19c068e20b5cc
ezzz3 <ezequielpretti@hotmail.com>
2024-12-13 23:31:46 -0300

    neww

---
+++ practica/ej7-ProyectoPlataformaBancaria/Usuario.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatosPersonas.Modelos
+{
+    public class DatosPersonales
+{
+    public string Nombre {get;set;}
+    public string Apellido {get;set;}
+    public string Direccion {get;set;}
+    public string Telefono {get;set;}
+    public string Correo {get;set;}
+    public int Edad {get;set;}
+
+    public DatosPersonales(string nombre, string apellido, string direccion, string telefono, string correo, int edad)
+    {
+        Nombre = nombre;
+        Apellido = apellido;
+        Direccion = direccion;
+        Telefono = telefono;
+        Correo = correo;
+        Edad = edad;
+        
+    }
+
+    public DatosPersonales()
+    {
+        Nombre = "";
+        Apellido = "";
+        Direccion = "";
+        Telefono = "";
+        Correo = "";
+        Edad = 0;
+
+    }
+}
+
+public class DatosDeTransaccion
+{
+    public int Ingresos {get;set;}
+    public int Gastos {get;set;}
+    public int MontoEspecifico {get;set;}
+    public string Fecha {get;set;}
+    public string Categoria {get;set;}
+    public string Descripcion {get;set;}
+
+    public DatosDeTransaccion(int ingresos, int gastos, int montoEspecifico, string fecha, string categoria, string descripcion)
+    {
+        Ingresos = ingresos;
+        Gastos = gastos;
+        MontoEspecifico = montoEspecifico;
+        Fecha = fecha;
+        Categoria = categoria;
+        Descripcion = descripcion;
+    }
+
+    public DatosDeTransaccion()
+    {
+        Ingresos = 0;
+        Gastos = 0;
+        MontoEspecifico = 0;
+        Fecha = "";
+        Categoria = "";
+        Descripcion = "";
+    }
+}
+}
+
+
